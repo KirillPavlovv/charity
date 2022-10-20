7e800c7c7f93139047d6876728ee95d304565a72
Kirill <kirillp1988@gmail.com>
2022-10-20 22:50:37 +0300

    Navbar fixed

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -25,8 +25,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 nav {
   padding: 30px;
 }
@@ -34,5 +34,9 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>

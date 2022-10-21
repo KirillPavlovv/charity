abf0144b27e8c59154738f8e11095be166fd696f
Kirill <kirillp1988@gmail.com>
2022-10-21 23:21:54 +0300

    add images

--- src/main/resources/db/changelog/changelog.sql
+++ src/main/resources/db/changelog/changelog.sql
@@ -49,22 +49,22 @@ VALUES ('474f1e0d-123a-4326-8e7c-5599a88220c5', 'Mait', 'Hunt', 'maint@gmail.com
 INSERT INTO products_category (id, name, price, image, is_edible)
 VALUES ('fde8d814-598e-4e4b-83b3-d612f3d7a31b', 'Brownie', 0.65, 'brownie.jpg', true);
 INSERT INTO products_category (id, name, price, image, is_edible)
-VALUES ('7af0f84d-e85c-4ea6-969d-f1a2d644a6d4', 'Muffin', 1.00, 'muffin.jpg', true);
+VALUES ('7af0f84d-e85c-4ea6-969d-f1a2d644a6d4', 'Muffin', 1.00, 'muffin.jpeg', true);
 INSERT INTO products_category (id, name, price, image, is_edible)
-VALUES ('879bc36c-e946-4673-a668-a48e24d21089', 'Cake Pop', 1.35, 'cake_pop.jpg', true);
+VALUES ('879bc36c-e946-4673-a668-a48e24d21089', 'Cake Pop', 1.35, 'cake_pop.jpeg', true);
 INSERT INTO products_category (id, name, price, image, is_edible)
-VALUES ('a9f4cfdb-ac39-4fb8-91d0-90e24d66d33e', 'Apple tart', 1.50, 'apple_tart.jpg', true);
+VALUES ('a9f4cfdb-ac39-4fb8-91d0-90e24d66d33e', 'Apple tart', 1.50, 'apple_tart.jpeg', true);
 INSERT INTO products_category (id, name, price, image, is_edible)
-VALUES ('067934b8-92cd-4b4a-8e45-f433f7066826', 'Water', 1.50, 'water.jpg', true);
+VALUES ('067934b8-92cd-4b4a-8e45-f433f7066826', 'Water', 1.50, 'water.jpeg', true);
 
 INSERT INTO products_category (id, name, price, image, is_edible)
-VALUES ('df6e78d8-045a-417f-b548-bf6d70a39ffa', 'Shirt', 2.00, 'shirt.jpg', true);
+VALUES ('df6e78d8-045a-417f-b548-bf6d70a39ffa', 'Shirt', 2.00, 'shirt.png', true);
 INSERT INTO products_category (id, name, price, image, is_edible)
-VALUES ('2f7e6726-89bf-4189-802a-d5ca7b083b2b', 'Pants', 3.00, 'pants.jpg', true);
+VALUES ('2f7e6726-89bf-4189-802a-d5ca7b083b2b', 'Pants', 3.00, 'pants.png', true);
 INSERT INTO products_category (id, name, price, image, is_edible)
-VALUES ('000287f2-5e7f-44aa-8045-12988b2be8b9', 'Jacket', 4.00, 'jacket.jpg', true);
+VALUES ('000287f2-5e7f-44aa-8045-12988b2be8b9', 'Jacket', 4.00, 'jacket.png', true);
 INSERT INTO products_category (id, name, price, image, is_edible)
-VALUES ('bc204e15-fcb2-456f-8353-521622d91e01', 'Toy', 1.00, 'toy.jpg', true);
+VALUES ('bc204e15-fcb2-456f-8353-521622d91e01', 'Toy', 1.00, 'toy.jpeg', true);
 
 INSERT INTO products_stock(id, quantity)
 VALUES ('fde8d814-598e-4e4b-83b3-d612f3d7a31b', 48);
--- ui/src/components/ProductComponent.vue
+++ ui/src/components/ProductComponent.vue
@@ -1,5 +1,8 @@
 <template>
   <base-card>
+    <a class="navbar-brand d-none d-sm-block" href="/">
+    <img :src="source" alt=""></a>
+
     <h3> Cake: {{ name }} </h3>
     <h4> Price: {{ price }} </h4>
     <a> Quantity: {{ quantity }}</a>
@@ -8,10 +11,16 @@
 
 <script>
 import BaseCard from "@/components/BaseCard";
+
 export default {
   name: "ProductComponent",
   components: {BaseCard},
-  props: ['id', 'name', 'price', 'quantity', 'image', 'isEdible']
+  props: ['id', 'name', 'price', 'quantity', 'image', 'isEdible'],
+  data() {
+    return {
+      source: '../assets/image/' + this.image,
+    }
+  },
 }
 </script>
 

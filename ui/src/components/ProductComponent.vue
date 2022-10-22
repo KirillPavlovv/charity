c613f3a9f5df4ab3f619c42aa98dc0b055b3c895
Kirill <kirillp1988@gmail.com>
2022-10-22 19:23:18 +0300

    calculate total amount to pay

--- ui/src/components/ProductComponent.vue
+++ ui/src/components/ProductComponent.vue
@@ -1,31 +1,27 @@
 <template>
-  <base-card>
     <div class="row">
       <div class="col">
-        <button id="button" @click="count()">
+        <button @click="count()">
           <img :src=image class="bg-image img-thumbnail rounded mx-auto d-block" style="width: 150px; height: 150px;"
-               :class="{mask: quantity<=0}" alt="photo">
+               :class="{mask: quantityAfterOrder<=0}" alt="photo">
         </button>
 
       </div>
       <div class="col">
         <h3> Cake: {{ name }} </h3>
-        <h4> Price: {{ price }} </h4>
-        <a> Quantity: {{ quantity }}</a>
+        <h4> Price: {{ price }} €</h4>
+        <p> Quantity: {{ quantityAfterOrder }}</p>
         <h5 v-show="itemsToBuy>0" style="color: red"> To buy: {{ itemsToBuy }}</h5>
-        <a v-show="totalSum>0" style="color: red"> To pay: {{ totalSum }}</a>
+        <p v-show="totalSum>0" style="color: red"> To pay: {{ parseFloat(totalSum).toFixed(2) }} € </p>
       </div>
     </div>
-  </base-card>
 </template>
 
 <script>
-import BaseCard from "@/components/BaseCard";
 import EventBus from "@/components/event-bus";
 
 export default {
   name: "ProductComponent",
-  components: {BaseCard},
   props: ['id', 'name', 'price', 'quantity', 'image', 'isEdible'],
   data() {
     return {
@@ -36,33 +32,42 @@ export default {
   },
   methods: {
     count() {
-      if (this.quantity > 0) {
+      if (this.quantityAfterOrder > 0) {
         this.itemsToBuy = this.itemsToBuy + 1;
         // eslint-disable-next-line vue/no-mutating-props
-        this.quantity = this.quantity - 1;
+        this.quantityAfterOrder = this.quantityAfterOrder - 1;
         if (this.quantity < 0) {
           // eslint-disable-next-line vue/no-mutating-props
           this.quantity = 0
         }
 
         this.totalSum = this.itemsToBuy * this.price
+        EventBus.$emit('payForItem', this.price)
       }
-      EventBus.$emit('payForItem', this.totalSum)
     },
   }
 }
 </script>
 
 <style scoped>
-#button {
-  border: white;
+button {
   padding: 0;
+  background-color: #ffffff;
+  border: 1px solid #ffffff;
 }
 
 .mask {
   opacity: 0.4;
   filter: alpha(opacity=40); /* msie */
   background-color: #000;
+}
+
+.row{
+  border-radius: 12px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  margin: 1rem auto;
+  max-width: 100rem;
 }
 
 </style>
--- ui/src/views/ProductsView.vue
+++ ui/src/views/ProductsView.vue
@@ -1,31 +1,50 @@
 <template>
   <section v-show="products.length>0">
-      <div class="controls"></div>
+    <base-card class="controls">
       <ul>
         <product-component v-for="product in products"
-                 :key="product.id"
-                 :id="product.id"
-                 :name="product.name"
-                 :price="product.price"
-                 :quantity="product.quantity"
-                 :image="product.image"
-                 :is-edible="product.isEdible"
+                           :key="product.id"
+                           :id="product.id"
+                           :name="product.name"
+                           :price="product.price"
+                           :quantity="product.quantity"
+                           :image="product.image"
+                           :is-edible="product.isEdible"
         ></product-component>
       </ul>
+    </base-card>
+    <base-card>
+      Total amount: {{ parseFloat(amountToPay).toFixed(2) }} €
+    </base-card>
+    <base-card>
+      <div class="row">
+        <div class="col">
+          <button class="btn btn-primary w-50 text-white mt-3 mr-1" v-on:click="getProducts">Reset</button>
+        </div>
+        <div class="col">
+          <button class="btn btn-primary w-50 text-white mt-3">Checkout</button>
+        </div>
+      </div>
+    </base-card>
   </section>
+
 </template>
 
 <script>
 
 import ProductComponent from "@/components/ProductComponent";
+import BaseCard from "@/components/BaseCard";
+import EventBus from "@/components/event-bus";
+
 export default {
   name: "ProductsView",
-  components: {ProductComponent},
+  components: {BaseCard, ProductComponent},
 
   data() {
     return {
       products: [],
       errorMessage: null,
+      amountToPay: 0,
     }
   },
 
@@ -41,14 +60,22 @@ export default {
             this.errorMessage = data.title;
           })
           .catch(error => console.error(error));
-    }
+    },
+
   },
   mounted() {
     this.getProducts();
+    EventBus.$on('payForItem', data => {
+      console.log(data)
+      this.amountToPay = this.amountToPay + data
+    })
   },
 }
 </script>
 
 <style scoped>
+button{
+
+}
 
 </style>
